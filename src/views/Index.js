a01169989fa3d8ba58e8fa3c68cf6be9246dd6d8
jimmy <JimmyMall@live.com>
2022-07-04 13:43:47 +0800

    v0.3.7-1@sust 完善闹钟 [2022.7.4]

--- src/views/Index.js
+++ src/views/Index.js
@@ -37,50 +37,81 @@ export default{
 			var canvas = document.getElementById('canvas-clock');
 			//获取context对象
 			var ctx = canvas.getContext('2d');
+			//Canvas 坐标系统: 左上角原点，x向左，y向下为正方向。
+			//Canvas 角度系统: x轴+方向为0，顺时针为角度正方向。使用弧度制，换算方法 Math.PI/180*角度
+
 			function drawFrame(){
 				//获取时间
-				var H=new Date().getHours();
-				var M=new Date().getMinutes();
-				var S=new Date().getSeconds();
+				var now=new Date();
+				var hour=now.getHours(); //24小时制
+				var minute=now.getMinutes();
+				var second=now.getSeconds();
 
-				//settings
-				var length_L=30;
-				var length_M=20;
+				//1. settings
+				var marginLength=10; //四边空白部分
+				var tickLengthL=30; //表盘刻度长度，长刻度 1-12
+				var tickLengthS=20; //表盘刻度长度，短刻度 1-2中间5格
+				var centerR=8; //中心点的半径
 
-				canvas.height = canvas.height // 清空画布
+				var ratioH=0.45; //时针半径与表盘的比例
+				var ratioM=0.65; //分针半径与表盘的比例
+				var ratioS=0.8; //秒针半径与表盘的比例
 
-				//2.晚上涂黑 18点后 6点前。
-				if(H >18 || H<6){
-					ctx.fillStyle="#000";
-				}else{
-					ctx.fillStyle="#FFF";
+				//(2).设置前景色、背景色
+				var fColor="#000"; //daylight
+				var bColor="#fff";
+				//晚上黑北京: 18点后 6点前。
+				if(hour >18 || hour<6){
+					fColor="#fff"; //night
+					bColor="#000";
 				}
-				// 如果是晚上，则涂黑背景
-				ctx.fillRect(0,0, canvas.width,canvas.height);
 
-				//3.圆形钟表外圈
-				var R=(canvas.width-10)/2;
-				var cX=(canvas.width-10)/2+5; //圆心的坐标
+
+				//2. begin to draw
+				// (1)清空画布
+				canvas.height = canvas.height;
+
+				//(2).圆形钟表外圈
+				var R=(canvas.width-marginLength)/2; //圆盘半径
+				var cX=(canvas.width-marginLength)/2+ marginLength/2; //圆心坐标
 				//描边
+				//ctx.save() //保存图形状态
 				ctx.beginPath();
-				ctx.lineWidth=3;
 				ctx.arc(cX,cX,R, 0, Math.PI*2);
 				ctx.closePath(); //结束路径
+				//
+				ctx.lineWidth=marginLength; //描边宽度
+				ctx.strokeStyle="#000";
 				ctx.stroke(); //描边
-				//填充
-				ctx.fillStyle="#FFF";
-				ctx.globalAlpha=0.7;
+				//
+				ctx.lineWidth=marginLength*0.9; //描边宽度
+				ctx.strokeStyle="#555";
+				ctx.stroke(); //描边
+				//
+				ctx.lineWidth=marginLength*0.5; //描边宽度
+				ctx.strokeStyle="#999";
+				ctx.stroke(); //描边
+				//
+				ctx.lineWidth=marginLength*0.2; //描边宽度
+				ctx.strokeStyle="#fff";
+				ctx.stroke(); //描边
+
+				//填充 背景色
+				ctx.fillStyle=bColor;
+				//ctx.globalAlpha=0.7;
 				ctx.fill();//白色表盘
 
-				//中心点
-				ctx.globalAlpha=1;
-				//ctx.fillStyle="#000";
+				//(3)指针中心点
+				//ctx.globalAlpha=1;
+				ctx.fillStyle=fColor;
 				ctx.beginPath();
-				ctx.arc(cX,cX, 5, 0, Math.PI*2);
+				ctx.arc(cX,cX, centerR, 0, Math.PI*2);
 				ctx.fill();
+				//ctx.restore() //保存图形状态
 
-				//时针描线,
-				var Rh=(canvas.width-length_L)/2; //时针内圈的半径
+				// (4)刻度线
+				//大刻度线
+				var Rh=(canvas.width-tickLengthL)/2; //时针内圈的半径
 				var angle=Math.PI/180; //PI*2==360度, 
 				for(var i=0; i<360; i+=30){
 					//console.log(i/30);
@@ -88,47 +119,66 @@ export default{
 					ctx.moveTo(cX+R*Math.cos(angle*i), cX+R*Math.sin(angle*i));
 					ctx.lineTo(cX+Rh*Math.cos(angle*i), cX+Rh*Math.sin(angle*i));
 					ctx.lineWidth=2;
+					ctx.strokeStyle=fColor;
 					ctx.stroke();
 				}
 
-				//小格子描线
-				var Rm=(canvas.width-length_M)/2; //小格子内圈的半径
+				//小刻度线
+				var Rm=(canvas.width-tickLengthS)/2; //小格子内圈的半径
 				for(var i=0; i<360; i+=6){
 					//console.log(i/6);
 					ctx.beginPath();
 					ctx.moveTo(cX+R*Math.cos(angle*i), cX+R*Math.sin(angle*i));
 					ctx.lineTo(cX+Rm*Math.cos(angle*i), cX+Rm*Math.sin(angle*i));
 					ctx.lineWidth=1;
+					ctx.strokeStyle=fColor;
 					ctx.stroke();
 				}
 
+				//小时数值 1-12
+				var r=0.85;
+				ctx.font="bold 25px Arial"; //文字样式
+				ctx.textAlign='center'; //文字对齐 center
+				ctx.textBaseline='middle';
+				ctx.fillStyle=fColor;
+				for(var i=0; i<360; i+=30){
+					var x=cX+Rh*Math.cos(angle*i)*r;
+					var y=cX+Rh*Math.sin(angle*i)*r;
+					//填充文字
+					var text=Math.round( (i/30+2)%12+1 ); //刻度值
+					ctx.fillText( text, x, y);//文字和位置
+				}
 
+
+				// (5) 指针
 				//画时针
-				var angle_pH=angle*(( H %12+M/60+S/3600)-3)/12*360;
+				var angle_pH=angle*(( hour %12+minute/60+second/3600)-3)/12*360;
 				ctx.beginPath();
-				ctx.moveTo(cX+0.5*R*Math.cos(angle_pH), cX+0.5*R*Math.sin(angle_pH));
+				ctx.moveTo(cX+ratioH*R*Math.cos(angle_pH), cX+ratioH*R*Math.sin(angle_pH));
 				ctx.lineTo(cX,cX);
 				ctx.lineWidth=5;
+				ctx.strokeStyle=fColor;
 				ctx.stroke();
 
 				//画分针
-				var angle_pM=angle*((M+S/60)%60-15)/60*360;
+				var angle_pM=angle*((minute+second/60)%60-15)/60*360;
 				ctx.beginPath();
-				ctx.moveTo(cX+0.7*R*Math.cos(angle_pM), cX+0.7*R*Math.sin(angle_pM));
+				ctx.moveTo(cX+ratioM*R*Math.cos(angle_pM), cX+ratioM*R*Math.sin(angle_pM));
 				ctx.lineTo(cX,cX);
-				ctx.lineWidth=2;
+				ctx.lineWidth=3;
+				ctx.strokeStyle=fColor;
 				ctx.stroke();
 
 				//画秒针
-				var angle_pS=angle*((S)%60-15)/60*360;
+				var angle_pS=angle*((second)%60-15)/60*360;
 				ctx.beginPath();
-				ctx.moveTo(cX+0.8*R*Math.cos(angle_pS), cX+0.8*R*Math.sin(angle_pS));
+				ctx.moveTo(cX+ratioS*R*Math.cos(angle_pS), cX+ratioS*R*Math.sin(angle_pS));
 				ctx.lineTo(cX,cX);
-				ctx.lineWidth=1;
+				ctx.lineWidth=2;
 				ctx.strokeStyle="#F00";
 				ctx.stroke();
 
-				//下一帧
+				//(6) 动画下一帧
 				//window.requestAnimationFrame(that.drawFrame) //报错
 				setTimeout( function(){
 					drawFrame()
